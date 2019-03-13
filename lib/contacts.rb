5a0ec98bb85ae6afd5ddaea2deb68607a2e7ffb9
Alberto Morales <cmtmorales24@gmail.com>
2019-03-13 16:34:15 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts["Freddy Mercury"].collect do |k,v| 
     if k == :favorite_ice_cream_flavors 
-     k.drop(1)
+     k.delete_at 0
     end 
   end 
 end
